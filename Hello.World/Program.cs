aa7cfc843e1b433e726250207ccc0867dbde03bd
ashwin027 <ashwin027@yahoo.com>
2015-10-04 18:42:30 -0700

    added user sanity check
    added more useless code

--- Hello.World/Program.cs
+++ Hello.World/Program.cs
@@ -12,6 +12,7 @@ namespace Hello.World
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("\r\nPress any key to exit!");
+            Console.WriteLine("\nand if you are searching for the any key, then please get help!!");
             Console.ReadKey();
         }
     }
