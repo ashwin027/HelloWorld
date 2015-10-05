7fa406a285b49ab62dc8b569e27576ab0f268ce0
Alex Shields <akashields@gmail.com>
2015-10-04 18:30:28 -0700

    Initial Check-in

---
+++ Hello.World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\r\nPress any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
